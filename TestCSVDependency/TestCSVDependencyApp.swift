6f2f78ec1feb1008a786aac80d139c7d051aee95
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-09-16 19:24:00 -0300

    Initial Commit

---
+++ TestCSVDependency/TestCSVDependencyApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestCSVDependencyApp.swift
+//  TestCSVDependency
+//
+//  Created by Luiz Araujo on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCSVDependencyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
